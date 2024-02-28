2926a04afe6d29bf5149dd8fd941f61c70cda04b
gibsonfriedman <gibsonafriedman@gmail.com>
2024-02-27 23:31:56 -0800

    partial

--- sql/04.sql
+++ sql/04.sql
@@ -4,3 +4,11 @@
  * HINT:
  * It's possible to solve this problem both with and without subqueries.
  */
+SELECT title FROM film WHERE film.film_id IN(
+        SELECT DISTINCT film.film_id FROM film
+        JOIN inventory ON film.film_id = inventory.film_id
+        JOIN rental ON inventory.inventory_id = rental.inventory_id
+	JOIN customer on rental.customer_id = customer.customer_id
+	WHERE customer.customer_id = 1
+	GROUP BY film.film_id HAVING count(*) > 1)
+ORDER BY title;
