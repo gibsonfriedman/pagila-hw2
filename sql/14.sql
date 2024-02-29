4df27ef84a3f4ca74ac722a872642c6af388081d
gibsonfriedman <gibsonafriedman@gmail.com>
2024-02-29 14:04:39 -0800

    more code

--- sql/14.sql
+++ sql/14.sql
@@ -5,3 +5,15 @@
  * This query is very similar to the previous problem,
  * but requires an additional JOIN.
  */
+SELECT
+    EXTRACT (YEAR FROM rental_date) "Year",
+    EXTRACT (MONTH FROM rental_date) "Month",
+    SUM (amount) "Total Revenue"
+FROM
+    rental
+    JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY
+    ROLLUP (
+        EXTRACT (YEAR FROM rental_date),
+        EXTRACT (MONTH FROM rental_date))
+ORDER BY "Year", "Month";
